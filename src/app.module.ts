ddf6a3b9c313f46cdb075626f40a4b42d170cded
Akzhan Sarzhanuly <akzhansarzhanuly@MacBook-Air-Akzhan-2.local>
2022-12-14 19:55:36 +0600

    fix build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+// import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -46,7 +46,7 @@ import { DeleteController } from './delete/delete.controller';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    // ConfigModule.forRoot(),
     MongooseModule.forRoot(
       settings.MONGO_URI +
         settings.MONGO_DB_NAME +
