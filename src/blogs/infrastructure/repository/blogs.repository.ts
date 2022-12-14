7246e98e8cfea3e67da7947881b809dd34a1bd2c
Akzhan Sarzhanuly <akzhansarzhanuly@MacBook-Air-Akzhan-2.local>
2022-12-14 22:06:16 +0600

    fix path blogs

--- src/blogs/infrastructure/repository/blogs.repository.ts
+++ src/blogs/infrastructure/repository/blogs.repository.ts
@@ -1,8 +1,8 @@
 import { BlogItemDBType, BlogItemType } from '../blogs.type';
 import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
-import { BlogsStateRepository } from 'src/blogs/application/blogs.interface';
 import { InjectModel } from '@nestjs/mongoose';
+import { BlogsStateRepository } from '../../application/blogs.interface';
 
 @Injectable()
 export class BlogsRepository implements BlogsStateRepository {
