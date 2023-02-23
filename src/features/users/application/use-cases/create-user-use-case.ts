521ba1f2cf430b5b77b3e2ae807ead1aea78d7f5
Akzhan Sarzhanuly <akzhansarzhanuly@MacBook-Air-Akzhan-2.local>
2023-02-23 21:18:10 +0600

    fix use case

--- src/features/users/application/use-cases/create-user-use-case.ts
+++ src/features/users/application/use-cases/create-user-use-case.ts
@@ -1,6 +1,5 @@
-import { Users, UsersDocument } from '../../domain/entity/users.schema';
+import { UsersDocument } from '../../domain/entity/users.schema';
 import { CreateUserDTO } from './../dto/users.dto';
-import { InjectModel } from '@nestjs/mongoose';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { UsersService } from '../users.service';
 
@@ -9,10 +8,7 @@ export class CreateUserCommand {
 }
 @CommandHandler(CreateUserCommand)
 export class CreateUserUseCase implements ICommandHandler<CreateUserCommand> {
-  constructor(
-    @InjectModel(Users.name)
-    protected usersService: UsersService,
-  ) {}
+  constructor(protected usersService: UsersService) {}
 
   async execute(command: CreateUserCommand): Promise<UsersDocument> {
     return this.usersService.createUser(command.createUserDTO);
