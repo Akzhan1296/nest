63d62a457515a5262d21508d6d17089646126dda
Akzhan Sarzhanuly <akzhansarzhanuly@MacBook-Air-Akzhan-2.local>
2023-04-12 12:14:39 +0600

    fix post input validation

--- src/features/blogs/api/models/input.models.ts
+++ src/features/blogs/api/models/input.models.ts
@@ -1,10 +1,10 @@
 import { Transform } from 'class-transformer';
-import { IsNotEmpty, Matches, MaxLength } from 'class-validator';
+import { IsNotEmpty, Matches, MaxLength, isNotEmpty } from 'class-validator';
 import { PageSizeDTO } from '../../../../common/common-types';
 
 export class BlogInputModelType {
-  @IsNotEmpty()
   @Transform(({ value }) => value.trim())
+  @IsNotEmpty()
   @MaxLength(15)
   name: string;
   @Matches('^https://([a-zA-Z0-9_-]+.)+[a-zA-Z0-9_-]+(/[a-zA-Z0-9_-]+)*/?$')
@@ -15,10 +15,16 @@ export class BlogInputModelType {
 }
 
 export class CreatePostByBlogIdInputType {
+  @Transform(({ value }) => value.trim())
+  @IsNotEmpty()
   @MaxLength(30)
   title: string;
+  @Transform(({ value }) => value.trim())
+  @IsNotEmpty()
   @MaxLength(100)
   shortDescription: string;
+  @Transform(({ value }) => value.trim())
+  @IsNotEmpty()
   @MaxLength(1000)
   content: string;
 }
